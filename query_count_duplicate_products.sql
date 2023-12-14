19b7108a58019993b235923604a925d910089100
jimetgithub <rahmat.wijaya@gmail.com>
2023-12-14 22:26:10 +0700

    Add files via upload

---
+++ query_count_duplicate_products.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT
+	product_new_title.title AS `Group Title`, 
+	COUNT(product_new_title.title) AS `Product Count`, 
+	product_new_title.created_at AS `Updated At`
+FROM
+	product_duplicate_lists
+	INNER JOIN
+	product_new_title
+	ON 
+		product_duplicate_lists.product_id = product_new_title.id
+GROUP BY product_new_title.title
+ORDER BY `Product Count` DESC
